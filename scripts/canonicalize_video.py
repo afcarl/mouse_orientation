019dade0b53d9fe3402f6235d8644db2e3175aeb
Matthew Johnson <mattjj@csail.mit.edu>
2016-02-13 12:30:45 -0500

    make canonicalize video open .pkl or .pkl.gz

--- scripts/canonicalize_video.py
+++ scripts/canonicalize_video.py
@@ -2,8 +2,14 @@ from __future__ import division
 import numpy as np
 import cPickle as pickle
 import sys
+import gzip
 
 from mouse_orientation.canonicalize import reorient_video
+
+def open_pkl_or_pklgz(filename, mode):
+    if filename.endswith('.gz'):
+        return gzip.open(filename, mode)
+    return open(filename, mode)
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -11,19 +17,21 @@ if __name__ == "__main__":
         sys.exit(1)
 
     nnet_params_filename, input_filename, output_filename = sys.argv[1:]
+    sigmasq_states = 0.1**2
 
     with open(nnet_params_filename, 'r') as infile:
         nnet_params = pickle.load(infile)
 
     print 'loading from {}...'.format(input_filename)
-    with open(input_filename, 'r') as infile:
+    with open_pkl_or_pklgz(input_filename, 'r') as infile:
         datadict = pickle.load(infile)
     print '...done!'
 
     # reorient each video
     print 're-orienting video sequences...'
-    reoriented_datadict = {name: reorient_video(nnet_params, frames)
-                           for name, frames in datadict.iteritems()}
+    reoriented_datadict = \
+        {name: reorient_video(nnet_params, frames, sigmasq_states)
+         for name, frames in datadict.iteritems()}
     print '...done!'
 
     # save the result
